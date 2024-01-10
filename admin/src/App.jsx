967778b7c6620f28570cd1307d81e2833cbc1983
Ahmed-Boukhallout <massoubouka5@gmail.com>
2024-01-10 12:07:12 +0100

    almost done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Admin from './adminpages/loging'
+import Dashboard from './adminpages/Dashboard'
+
 function App() {
   return (
     <div className="App">
-      <Admin/>
+      <Router>
+   <Routes>
+     <Route path='/' element={<Admin/>}></Route>
+     <Route path='/Dashboard' element={<Dashboard/>}></Route>
+    </Routes>
+    </Router>
     </div>
   );
 }
