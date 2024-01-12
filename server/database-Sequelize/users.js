9cdf757f0038baba3dabb0500164d6118d8375cf
azizchannoufi <channoufi2003@gmail.com>
2024-01-12 09:20:05 +0100

    donenow

--- server/database-Sequelize/users.js
+++ server/database-Sequelize/users.js
@@ -2,27 +2,27 @@ const Users =(connection,DataTypes)=>{
     return connection.define("users", {
    user_name: {
      type: DataTypes.STRING,
-     allowNull: true,
+     allowNull: false,
    },
    user_phone:{
     type: DataTypes.INTEGER,
-    allowNull: true,
+    allowNull: false,
    },
    user_Pseudo: {
      type: DataTypes.STRING,
-     allowNull: true,
+     allowNull: false,
    },
    user_password: {
      type: DataTypes.STRING,
-     allowNull: true,
+     allowNull: false,
    },
    user_role: {
     type: DataTypes.STRING,
-    allowNull: true,
+    allowNull: false,
   },
   user_location :{
     type: DataTypes.STRING,
-    allowNull: true,
+    allowNull: false,
   }
  
  })
